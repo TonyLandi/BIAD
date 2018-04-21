eb9f25923f4b7ee66ad0768a1ffe33a73df59b7e
Tony Landi <tony.landi@lcnonline.net>
2018-04-21 09:40:36 -0500

    Lab 2.1 code cleanup

--- Lab 2/Code Snippets/Lab 2.1/MessagesController.cs
+++ Lab 2/Code Snippets/Lab 2.1/MessagesController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Http;
 using AdaptiveCards;
 using Autofac;
+using EchoBot.Dialogs;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Connector;
@@ -26,7 +27,8 @@ namespace Lab_2_1_Dialogs_Bot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new RootDialog());
+                //await Conversation.SendAsync(activity, () => new Dialogs.QnaDialog());
             }
             else
             {
@@ -38,32 +40,55 @@ namespace Lab_2_1_Dialogs_Bot
 
         private async Task HandleSystemMessage(Activity message)
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
+            switch (message.Type)
             {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                IConversationUpdateActivity update = message;
-                using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, message))
-                {
-                    var client = scope.Resolve<IConnectorClient>();
-                    if (update.MembersAdded.Any())
+                // Implement when a user joins the conversation
+                case ActivityTypes.ContactRelationUpdate:
+                    // The bot is always added as a user in the conversation,
+                    // Since we don't want the adaptive card to display twice, 
+                    // we'll ignore the second conversation that is triggered by the bot.
+                    break;
+
+                // Implement when a user leaves the conversation
+                case ActivityTypes.DeleteUserData:
+                    // If we handle user deletion, return a real message
+                    break;
+
+                // The user is typing
+                case ActivityTypes.Typing:
+                    break;
+
+                // Bounce a message off of the server without replying or changing it's state
+                case ActivityTypes.Ping:
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+
+                    // The referenced conversation is being updated
+                    IConversationUpdateActivity update = message;
+
+                    // Within the scope of this conversation
+                    using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, message))
                     {
-                        var reply = message.CreateReply();
-                        foreach (var newMember in update.MembersAdded)
+                        var client = scope.Resolve<IConnectorClient>();
+                        if (update.MembersAdded.Any())
                         {
-                            // the bot is always added as a user of the conversation, since we don't
-                            // want to display the adaptive card twice ignore the conversation update triggered by the bot
-                            if (newMember.Name.ToLower() != "bot")
+                            var reply = message.CreateReply();
+                            foreach (var newMember in update.MembersAdded)
                             {
+                                // the bot is always added as a user of the conversation, since we don't
+                                // want to display the adaptive card twice ignore the conversation update 
+                                // triggered by the bot
+                                if (newMember.Name.ToLower() == "bot") continue;
+                            
                                 try
                                 {
                                     // read the json in from our file
-                                    string json = File.ReadAllText(HttpContext.Current.Request.MapPath("~\\AdaptiveCards\\MyCard.json"));
+                                    var json = File.ReadAllText(HttpContext.Current.Request.MapPath("~\\MyCard.json"));
+
                                     // use Newtonsofts JsonConvert to deserialized the json into a C# AdaptiveCard object
-                                    AdaptiveCards.AdaptiveCard card = JsonConvert.DeserializeObject<AdaptiveCards.AdaptiveCard>(json);
+                                    var card = JsonConvert.DeserializeObject<AdaptiveCard>(json);
+
                                     // put the adaptive card as an attachment to the reply message
                                     reply.Attachments.Add(new Attachment
                                     {
@@ -80,19 +105,9 @@ namespace Lab_2_1_Dialogs_Bot
                             }
                         }
                     }
-                }
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
+
+                    break;
+
             }
         }
     }
